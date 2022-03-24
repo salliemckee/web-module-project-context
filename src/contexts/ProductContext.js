2f2f9bb49bc358bbabbde9fc77a4f819145a40f1
Sallie McKee <salliemckee@Morrigan.local>
2022-03-24 15:43:58 -0500

    finished setting up projectContext

---
+++ src/contexts/ProductContext.js
@@ -0,0 +1,19 @@
+import React, { createContext } from "react";
+
+// const stuff = {
+//   cart: [],
+//   products: [],
+//   addItem: () => {
+//     console.log("test");
+//   },
+// };
+
+export const ProductContext = createContext();
+
+// export default function ProductProvider(props) {
+//   return (
+//     <ProductContext.Provider value={stuff}>
+//       {props.children}
+//     </ProductContext.Provider>
+//   );
+// }
